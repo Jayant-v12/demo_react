77bd8a221e847fae2f059fe461add2d542598ce3
JAYANT <jayants938@gmail.com>
2026-03-17 07:35:58 +0000

    clac

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -4,11 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function fun() {
+    return "inside the function";
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  function calc(a, b, op) {
+    if (op === "+") {
+      return a + b;
+    } else if (op === "-") {
+      return a - b;
+    } else {
+      return a * b;
+    }
+  }
+
+  const name = "John";
+  let x = 10;
+  let y = 20;
 
   return (
-      <h1>HELLO THIS IS MY FIRST PROJECT</h1>
+    <div>
+      <h1>User Variable</h1>
+      <h3>Value of variable</h3>
+      <h4>{x + y}</h4>
+      <h3>{fun()}</h3>
+      <h3>Parameter function: {sum(30, 67)}</h3>
+
+      <h3>Calc (+): {calc(10, 5, "+")}</h3>
+      <h3>Calc (-): {calc(10, 5, "-")}</h3>
+      <h3>Calc (*): {calc(10, 5, "*")}</h3>
+    </div>
   )
 }
 
-export default App
+export default App;
